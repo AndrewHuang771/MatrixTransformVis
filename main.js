d34fb403c9fa77050624dc0e397a4fc1292d1bff
Andrew Huang <ahuang771@gmail.com>
2019-07-20 10:45:50 -0400

    Added basis vectors

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+const duration = 500;
+
 let basis = {
 	x1 : 100,
 	x2 : 0,
 	y1 : 0,
 	y2 : 100,
 };
+
+let oldBasis = {
+	x1 : 100,
+	x2 : 0,
+	y1 : 0,
+	y2 : 100,
+}
 
 function Render() {
 	RenderPoints();
@@ -14,8 +23,21 @@ function RenderPoints() {
 	$('.point').each(function(i, obj) {
     	let x = obj.getAttribute('data-x');
     	let y = obj.getAttribute('data-y');
-    	obj.style.top = x*basis.x1 + y*basis.x2 + 'px';
-    	obj.style.left = x*basis.y1 + y*basis.y2 + 'px';
+    	let oldX = x*oldBasis.x1 + y*oldBasis.x2;
+    	let oldY = x*oldBasis.y1 + y*oldBasis.y2;
+    	let newX = x*basis.x1 + y*basis.x2;
+    	let newY = x*basis.y1 + y*basis.y2;
+    	let xStep = (newX - oldX) / (duration / 10);
+    	let yStep = (newY - oldY) / (duration / 10);
+    	let j = 1;
+    	let animation = setInterval(function() {
+    		obj.style.top = oldX + xStep*j + 'px';
+    		obj.style.left = oldY + yStep*j + 'px';
+    		j += 1;
+    		if ( j === duration / 10 + 1 ) {
+    			clearInterval( animation );
+    		}
+    	}, 10 );
 	});
 }
 
@@ -23,15 +45,30 @@ function RenderVectors() {
 	$('.vector').each(function(i, obj) {
     	let x = obj.getAttribute('data-x');
     	let y = obj.getAttribute('data-y');
+    	let oldX = x*oldBasis.x1 + y*oldBasis.x2;
+    	let oldY = x*oldBasis.y1 + y*oldBasis.y2;
     	let newX = x*basis.x1 + y*basis.x2;
     	let newY = x*basis.y1 + y*basis.y2;
-    	let magnitude = Math.sqrt( newX*newX + newY*newY );
-    	let angle = (Math.atan2( newX, newY )*180/Math.PI);
-    	obj.style.width = magnitude + 'px';
-		obj.style.top = newX/2 + 'px';
-		obj.style.left = newY/2 + 'px';
-		obj.style.transform = "translate(-50%, -50%)";
-    	obj.style.transform += "rotate(" + angle + "deg)";
+    	let xStep = (newX - oldX) / (duration / 10);
+    	let yStep = (newY - oldY) / (duration / 10);
+    	let j = 1;
+
+    	let animation = setInterval(function() {
+    		newX = oldX + xStep*j;
+    		newY = oldY + yStep*j;
+    		j++;
+    		let magnitude = Math.sqrt( newX*newX + newY*newY );
+	    	let angle = (Math.atan2( newX, newY )*180/Math.PI);
+	    	obj.style.width = magnitude + 'px';
+			obj.style.top = newX/2 + 'px';
+			obj.style.left = newY/2 + 'px';
+			obj.style.transform = "translate(-50%, -50%)";
+	    	obj.style.transform += "rotate(" + angle + "deg)";
+    		if ( j === duration / 10 + 1 ) {
+    			clearInterval( animation );
+    			oldBasis = basis;
+    		}
+    	}, 10 );
 	});
 }
 
@@ -54,23 +91,42 @@ $(document).ready(function() {
 			$('.wrapper').append($point);
 		}
 	}
-	setTimeout(function() {
-		basis = {
-			x1 : 0,
-			x2 : 0,
-			y1 : 0,
-			y2 : 100,
-		};
-		Render();
-	}, 1000);
+	for ( let i = -10; i <= 10; i ++ ) {
+		let $gridline = $('<div>').addClass('gridlineV');
+		$gridline.attr('id', 'gridline' + i);
+		$gridline.css({
+			'left': i*100 + 'px',
+		});
+		if ( i == 0 ) {
+			$gridline.css({
+				'background-color': 'red',
+			});
+		}
+		$('.wrapper').append($gridline);
+	}
+	for ( let i = -10; i <= 10; i ++ ) {
+		let $gridline = $('<div>').addClass('gridlineH');
+		$gridline.attr('id', 'gridline' + i);
+		$gridline.css({
+			'top': i*100 + 'px',
+		});
+		if ( i == 0 ) {
+			$gridline.css({
+				'background-color': 'red',
+			});
+		}
+		$('.wrapper').append($gridline);
+	}
+
+	Render();
 
 	$("#basis_form").submit(function(e) {
 	    e.preventDefault();
 	    basis = {
-	    	x1: parseInt(document.getElementById('x1').value, 10),
-	    	y1: parseInt(document.getElementById('y1').value, 10),
-	    	x2: parseInt(document.getElementById('x2').value, 10),
-	    	y2: parseInt(document.getElementById('y2').value, 10),
+	    	x1: parseInt(document.getElementById('x1').value, 10)*100,
+	    	y1: parseInt(document.getElementById('y1').value, 10)*100*(-1),
+	    	x2: parseInt(document.getElementById('x2').value, 10)*100,
+	    	y2: parseInt(document.getElementById('y2').value, 10)*100,
 	    }
 	    Render();
 	});
